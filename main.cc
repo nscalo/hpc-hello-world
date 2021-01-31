b61be67b85a9cb2194446c6dff8a9b51d1a45098
Aswin Vijayakumar <aswinkvj@gmail.com>
2021-01-31 13:09:38 +0000

    compiler hello world

--- main.cc
+++ main.cc
@@ -4,6 +4,8 @@ int main() {
 #ifdef __INTEL_COMPILER
   // Only compiled with Intel Compiler
   printf("Hello world from Intel compiler");
+  int value = 1<<17;
+  printf("%d", value);
 #elif __GNUC__
   // Only compiled with GNU Compiler
   printf("Hello world from GNU compiler");
